f67a404711ff1c19cde777536ec03e98434b5c6e
miro <mgr.miroslav.murar@gmail.com>
2021-01-15 21:28:59 +0100

    objects

--- js-scripty.js
+++ js-scripty.js
@@ -16,3 +16,108 @@ if (age === joeAge) {
 } else {
     console.log("NOT SAME DATA TYPE or VALUE!!")
 }
+
+
+var students = ["Tim", "Joe", "Nick"]; 
+//var newField = [];
+//newField.push(students[0]); 
+//newField.push(students[1]);
+var index = students.indexOf("Joe"); 
+//if (index != 1) {
+//    console.log("Joe's index !=1")
+//} else {
+//    console.log("Joe's index ==1"); 
+//}
+
+console.log(students); 
+students.splice(index, 1); 
+
+console.log(students);
+
+
+var x = function(z, y) {
+    return z + y; 
+}
+
+console.log(x());
+
+var array = []; 
+array.push(x(12,43));
+array.push(x(22,11));
+array.push(x(3,6));
+array.push(x(8,77));
+
+console.log(array); 
+
+
+
+function myFunc(x, y) {
+    return x * y;
+}
+
+
+console.log(myFunc(4,5));
+
+
+
+//var student = {
+//    firstName: "John", 
+//    lastName: "Parker", 
+//    age: 7
+//};
+//
+//console.log(student.firstName);
+//console.log(student.lastName);
+
+
+
+
+
+//var Student = {
+//    firstName: "John", 
+//    lastName: "Parker", 
+//    age: 7, 
+//    greeting: function() {
+//        return "Hi I am " + this.firstName + " and I am " + this.age + " years old";
+//    }
+//};
+//
+//console.log(Student.greeting());
+//console.log(Student["firstName"]); 
+//console.log(Student["lastName"]); 
+
+
+
+
+
+function Student(firstName, lastName, age) {
+    this.firstName = firstName; 
+    this.lastName = lastName; 
+    this.age = age; 
+    this.greeting = function() {
+        return "Hi I am " + this.firstName + " and I am " + this.age + " years old";
+    }; 
+}
+
+var students = [];
+students.push(new Student("Jenny", "Laga", 5));
+students.push(new Student("Timmy", "Turner", 6));
+students.push(new Student("Carl", "Jr", 9));
+
+for (var index = 0;  index < students.length; index++) {
+    var student = students[index]; 
+    console.log(student.greeting());
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
